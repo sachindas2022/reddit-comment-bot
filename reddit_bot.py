15e0dedb4cdd78bb250ff08e282d799f4cd68f09
DELL <sachin.das.new@gmail.com>
2022-07-22 15:23:22 +0530

    Added Sleep Mode with Sleep Predefiend Module.

--- reddit_bot.py
+++ reddit_bot.py
@@ -5,14 +5,19 @@ import os
 
 def bot_login():
 	print("Logging in...")
-	r =praw.Reddit(username = config.username,
+	r=praw.Reddit(username = config.username,
 				password = config.password,
 				client_id = config.client_id,
 				client_secret = config.client_secret,
 				user_agent = "The Reddit Commenter v1.0")
-	print("Logged in!")
+	if print(True):
+		print("Logged in!")
+	else:
+		print('Try Again......')
 
 	return r
+def comments_replied_to():
+	print("For Testing")
 
 def run_bot(r, comments_replied_to):
 	print ("Searching last 1,000 comments")
